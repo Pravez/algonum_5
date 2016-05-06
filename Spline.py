f934336dfcc6020dff0f5bbb0c05690c224f2c1f
Pravez <paul.ravezb@gmail.com>
2016-05-07 00:31:46 +0200

    Spline marche totalement bien normalement

--- Spline.py
+++ Spline.py
@@ -1,7 +1,7 @@
 from airfoil_load import *
 import matplotlib.pyplot as plt
 
-(ex,ey,ix,iy) = load_foil('HOR20.DAT')
+(ex,ey,ix,iy) = load_foil('data/goe144.dat')
 
 def spline(x, y, yp1, ypn, max):
 
@@ -11,7 +11,6 @@ def spline(x, y, yp1, ypn, max):
 
     if(yp1 > max):
         y2[0] = u[0] = 0.0
-
     else:
         y2[0] = -0.5
         u[0] = (3.0 / (x[1] - x[0])) * ((y[1] - y[0]) / (x[1] - x[0]) - yp1)
@@ -25,7 +24,6 @@ def spline(x, y, yp1, ypn, max):
 
     if (ypn > max):
         qn = un = 0.0
-
     else:
         qn = 0.5
         un = (3.0 / (x[n] - x[n-1])) * (ypn - (y[n] - y[n-1]) / (x[n] - x[n-1]))
@@ -39,8 +37,8 @@ def spline(x, y, yp1, ypn, max):
 
 
 def splint(xa, ya, y2a, n, x):
-    klo = 1
-    khi = n
+    klo = 0
+    khi = n-1
     while khi-klo > 1 :
         k=(khi+klo) >> 1
         if(xa[k] > x):
@@ -56,41 +54,30 @@ def splint(xa, ya, y2a, n, x):
     y = a * ya[klo] + b * ya[khi] + ((a * a * a - a) * y2a[klo] + (b * b * b - b) * y2a[khi]) * (h * h) / 6.0
     return y
 
-#plt.plot(ix, iy)
-#for i in range(ix.size):
-#    plt.scatter(ix[i], iy[i])
-#plt.show()
 
-values = []
-current = 0
-i=0
-while(current < ix[ix.size-1] and i < ix.size):
-    current = 1e-3*i
-    values.append(current)
-    i+=1
+def compute_smthing_dos(xa, ya, npoints):
+    n = len(xa)
 
-print(values)
-print(ix)
+    value = (xa[n-1] / n) / npoints
+    total_points = npoints * n + 1
 
-xa = []
-ya = []
-for i in range (ix.size):
-    if(iy[i]> 1e-30):
-        ya.append(iy[i])
-        xa.append(ix[i])
+    #first_d = (ya[1] - ya[0])/(xa[1] - xa[0])
+    #last_d = (ya[n-1] - ya[n-2]) / (xa[n-1] - xa[n-2])
+
+    y2a = spline(xa, ya, 1e30, 1e30, 0.99e30)
+
+    final_points_y = []
+    final_points_x = []
+
+    for i in range(0, total_points):
+        final_points_x.append(value*i)
+        final_points_y.append(splint(xa, ya, y2a, n, value*i))
+
+    return [final_points_x, final_points_y]
 
 
-yp1 = (ya[1] - ya[0])/(xa[1] - xa[0])
-ypn = (ya[len(ya)-1] - ya[len(ya)-2]) / (xa[len(xa)-1] - xa[len(xa)-2])
-y2a = spline(xa, ya, yp1, ypn, 1e-30)
-
-
-d1 = []
-for i in range(len(values)):
-    d1.append(splint(xa, ya, y2a, len(xa)-1, values[i]))
-
-plt.plot(values, d1)
-plt.plot(xa, ya, color='red')
+extrados = compute_smthing_dos(ex, ey, 10)
+intrados = compute_smthing_dos(ix, iy, 10)
+plt.plot(extrados[0], extrados[1])
+plt.plot(intrados[0], intrados[1])
 plt.show()
-
-#il faut calculer les derivees premieres aux deux premiers points et aux deux derniers
