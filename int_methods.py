880085bcb03fa5d52cc2a3a7f3c46309b54df272
pgaulier <paul.gaulier@free.fr>
2016-05-07 16:10:59 +0200

    modification des methodes d'int, et du calcul de la longueur du graphe

--- int_methods.py
+++ int_methods.py
@@ -1,53 +1,66 @@
-def rect_r(f, a, b, n):
-    h = (b - a)/float(n)
+def rect_r(xi, yi):
     S = 0
-    for k in range(1, n+1):
-        S += f(a + k*h)
+    n = len(xi)
+    for i in range(1, n):
+        S += yi[i]
+    h = (xi[n - 1] - xi[0])/float(n)
     return h*S
 
 
-def rect_l(f, a, b, n):
-    h = (b - a)/float(n)
+def rect_l(xi, yi):
     S = 0
-    for k in range(0, n):
-        S += f(a + k*h)
+    n = len(xi)
+    for i in range(0, n - 1):
+        S += yi[i]
+    h = (xi[n - 1] - xi[0])/float(n)
     return h*S
 
 
-def midpoint(f, a, b, n):
-    h = (b - a)/float(n)
+def midpoint(xi, yi):
+    n = len(xi)
+    h = (xi[n - 1] - xi[0])/n
     S = 0
-    for k in range(0, n+1):
-        S += f(a + k*h + h/2.0)
+    for i in range(0, n - 1):
+        S += (yi[i] + yi[i + 1])/2.0
     return h*S
 
 
-def trapezoidal(f, a, b, n):
-    h = (b - a)/float(n)
+def trapezoidal(xi, yi):
+    n = len(xi)
+    h = (xi[n - 1] - xi[0])/float(n)
     S = 0
-    m = (f(a) + f(b))/2.0
-    for k in range(1, n):
-        S += f(a + k*h)
+    m = (xi[0] + xi[n - 1])/2.0
+    for i in range(1, n):
+        S += yi[i]
     return h*(m + S)
 
 
-def simpson(f, a, b, n):
-    h = (b - a)/float(n)
+def find_abscissa(xi, val):
+    for i in range(len(xi)):
+        if val < xi[i]:
+            return i - 1
+    return len(xi)
+
+
+def simpson(xi, yi):
+    n = len(xi)
+    h = (xi[n - 1] - xi[0])/float(n)
     S = 0
-    a0 = a
-    for k in range(n):
-        S += (h/6.)*(f(a0) + 4*f((2*a0+h)/2.) + f(a0 + h))
-        a0 += h
+    for i in range(n - 1):
+        x2 = (2 * xi[i] + h)/2.
+        ind = find_abscissa(xi, x2)
+        S += (h/6.) * (yi[i] + 4 * yi[ind] + yi[i + 1])
     return S
 
 
-# def f(x):
-#     return x
-
-# print(rect_r(f, 0, 1, 10))
-# print(rect_l(f, 0, 1, 10))
-# print(midpoint(f, 0, 1, 10))
-# print(trapezoidal(f, 0, 1, 10))
-# print(simpson(f, 0, 1, 10))
+'''
+xi = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+yi = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+print(rect_r(xi, yi))
+print(rect_l(xi, yi))
+print(midpoint(xi, yi))
+print(trapezoidal(xi, yi))
+print(simpson(xi, yi))
 
 ### La valeur exacte est 0.5 ###
+'''
--- length.py
+++ length.py
@@ -1,4 +1,5 @@
 from airfoil_load import *
+from int_methods import *
 import numpy as np
 import matplotlib.pyplot as mp
 
@@ -26,3 +27,39 @@ def derivate(Pol):
         
     return dPol
 
+
+def deriv(xi, yi):
+    '''
+    Takes a function f represented by an array of abscissas and an array of images and returns an array of the same length representing f'
+    :param xi: an array of abscissas
+    :param yi: an array of values of f corresponding to the abscissas xi
+    :return: an array of values of f' corresponding to the abscissas xi
+    '''
+    n = len(xi)
+    f_der = [yi[0]]
+    for i in range(1, n - 1):
+        pente = (yi[i + 1] - yi[i - 1])/(xi[i + 1] - xi[i - 1])
+        f_der.append(pente)
+    f_der.append(yi[n-1])
+    return f_der
+
+
+def length2(xi, yi, method):
+    '''
+    Returns the length of the graph of f between x and y based on the formula given in the subject, the integration method can be chosen
+    :param xi: an array representing the abscissas
+    :param yi: an array reprensenting the values of f at each point of xi
+    :param method: a function
+    :return: the length of f's graph
+    '''
+    df = deriv(xi, yi)
+    integrand = []
+    for i in range(len(xi)):
+        integrand.append(1 + df[i]**2)
+    return method(xi, integrand)
+
+
+xi = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+yi = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+print(deriv(xi, yi))
+print(length2(xi, yi, simpson))
